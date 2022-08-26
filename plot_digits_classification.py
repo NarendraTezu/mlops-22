48a9dfab53382acbb53350b8b791a1fa2a9c1756
NarendraTezu <narendra.tezu18@gmail.com>
2022-08-26 19:30:26 +0530

    This is my new commit in develop branch

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import ConfusionMatrixDisplay
 ###############################################################################
 # Digits dataset
 # --------------
